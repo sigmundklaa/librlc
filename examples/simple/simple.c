99f39be923e6f711981626d3a28327cc82f4e2e9
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-01-31 23:42:53 +0100

    simple example application

---
+++ examples/simple/simple.c
@@ -0,0 +1,162 @@
+
+#include <rlc/rlc.h>
+#include <rlc/utils.h>
+#include <semaphore.h>
+#include <stdlib.h>
+
+#define MTU (50)
+
+#define PACKET_LOSS_RATE (50)
+
+struct client {
+        sem_t sem;
+        pthread_t thread;
+
+        rlc_context ctx;
+
+        struct client *other;
+};
+
+static const struct rlc_config config = {
+        .window_size = 5,
+        .buffer_size = 5200,
+        .byte_without_poll_max = 500,
+        .pdu_without_poll_max = 3,
+        .sn_width = RLC_SN_12BIT,
+        .time_reassembly_us = 5000,
+        .time_poll_retransmit_us = 500,
+};
+
+static void *transmitter(void *client_arg)
+{
+        rlc_errno status;
+        struct client *cl;
+
+        cl = client_arg;
+
+        for (;;) {
+                status = sem_wait(&cl->sem);
+                if (status != 0) {
+                        rlc_errf("sem_wait: %" RLC_PRI_ERRNO, status);
+                        continue;
+                }
+
+                rlc_tx_avail(&cl->ctx, MTU);
+        }
+
+        return NULL;
+}
+
+static rlc_errno tx_request(struct rlc_context *ctx)
+{
+        struct client *cl = rlc_user_data(ctx);
+
+        (void)sem_post(&cl->sem);
+        return 0;
+}
+
+static rlc_errno tx_submit(struct rlc_context *ctx,
+                           const struct rlc_chunk *chunks)
+{
+        struct client *cl = rlc_user_data(ctx);
+
+        if (random() % 100 >= PACKET_LOSS_RATE) {
+                rlc_rx_submit(&cl->other->ctx, chunks);
+        } else {
+                rlc_wrnf("Dropping packet");
+        }
+
+        (void)tx_request(ctx);
+
+        return 0;
+}
+
+static void event(struct rlc_context *ctx, const struct rlc_event *event)
+{
+        switch (event->type) {
+        case RLC_EVENT_RX_DONE:
+                rlc_inff("RX done");
+                return;
+        case RLC_EVENT_TX_DONE:
+                rlc_inff("TX done");
+                return;
+        case RLC_EVENT_RX_FAIL:
+                rlc_errf("RX failure");
+                return;
+        }
+}
+
+static void *mem_alloc(struct rlc_context *ctx, size_t size)
+{
+        (void)ctx;
+        return malloc(size);
+}
+
+static void mem_dealloc(struct rlc_context *ctx, void *mem)
+{
+        (void)ctx;
+        free(mem);
+}
+
+static const struct rlc_methods methods = {
+        .tx_request = tx_request,
+        .tx_submit = tx_submit,
+        .event = event,
+        .mem_alloc = mem_alloc,
+        .mem_dealloc = mem_dealloc,
+};
+
+static void client_init(struct client *cl, struct client *other)
+{
+        rlc_errno status;
+
+        cl->other = other;
+
+        status = rlc_init(&cl->ctx, RLC_AM, &config, &methods, cl);
+        if (status != 0) {
+                rlc_panicf(status, "rlc_init");
+        }
+
+        status = sem_init(&cl->sem, 0, 0);
+        if (status != 0) {
+                rlc_panicf(status, "sem_init");
+        }
+
+        status = pthread_create(&cl->thread, NULL, transmitter, cl);
+        if (status != 0) {
+                rlc_panicf(status, "pthread_create");
+        }
+}
+
+#define MESSAGE                                                                \
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "     \
+        "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " \
+        "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "  \
+        "aliquip ex ea commodo consequat. Duis aute irure dolor in "           \
+        "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " \
+        "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "   \
+        "culpa qui officia deserunt mollit anim id est laborum."
+
+int main(void)
+{
+        static struct client client1;
+        static struct client client2;
+        struct rlc_chunk chunk;
+
+        rlc_plat_init();
+
+        srandom(time(NULL));
+
+        client_init(&client1, &client2);
+        client_init(&client2, &client1);
+
+        chunk.next = NULL;
+        chunk.data = MESSAGE;
+        chunk.size = sizeof(MESSAGE);
+
+        for (;;) {
+                (void)rlc_send(&client1.ctx, &chunk);
+
+                sleep(1);
+        }
+}
