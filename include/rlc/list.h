645e13668f45c2b5fedbb7ea63b4f552d3eb0cf1
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-01-26 17:23:21 +0100

    list: Remove rlc_list_foreach_safe
    Dodgy implementation, difficult to achieve desired behaviour. Manual
    control of the iterator is preferred in edge cases.

--- include/rlc/list.h
+++ include/rlc/list.h
@@ -44,37 +44,6 @@ static inline void rlc_list_node_init(rlc_list_node *node)
         for (it_ = rlc_list_it_init(list_); !rlc_list_it_eoi(it_);             \
              it_ = rlc_list_it_next(it_))
 
-#define rlc_list_it_safe__(name_) rlc_list_it_##name_##__LINE__##_safe__
-
-/**
- * @brief Iterate over @p list_, keeping the iterator in @p it_. It is safe to
- * remove using the iterator @p it_
- *
- * This expression is quite ugly, so here goes an explanation:
- * The for loop is, as any other for loop, split into three parts:
- * initialization, conitional check and continuation. Since we want the
- * iterator to be outside the scope of the loop, for example for inserting at
- * the end, we need to use the initialization to declare the next iterator.
- * However, since we declare the variable here we can't also assign to @p it_
- * as we would normally. So, we make use of the not so well known comma
- * operator, which works as below:
- * int a = (b, c); // evaluates to a = c
- *
- * The conditional check simply checks if we are at end of iteration. The
- * continuation step stores the previously loaded next iterator into @p it_,
- * and loads the next value now, instead of at the end of the next iteration.
- *
- * This behaviour makes it so  that we don't rely on the current value to
- * get the next, so the current value can be removed and/or deallocated while
- * still continuing to iterate safely.
- */
-#define rlc_list_foreach_safe(list_, it_)                                      \
-        for (struct rlc_list_it rlc_list_it_safe__(next) =                     \
-                     (it_ = rlc_list_it_init(list_), rlc_list_it_next(it_));   \
-             !rlc_list_it_eoi(it_); it_ = rlc_list_it_safe__(next),            \
-                                rlc_list_it_safe__(next) = rlc_list_it_next(   \
-                                        rlc_list_it_safe__(next)))
-
 static inline rlc_list_it rlc_list_it_init(rlc_list *list)
 {
         return (struct rlc_list_it){
