4d0637b7306474fca37db042d2cdb5018a5153fb
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-02-21 11:52:54 +0100

    arq: Fix retransmit segment start overflow

--- src/arq.c
+++ src/arq.c
@@ -629,7 +629,7 @@ static size_t tx_poll(struct rlc_context *ctx, size_t max_size)
                 }
 
                 seg.end = last_seg->seg.end;
-                seg.start = seg.end - rlc_min(seg.end - seg.start,
+                seg.start = seg.end - rlc_min(seg.end - last_seg->seg.start,
                                               max_size - header_size);
 
                 gabs_log_dbgf(ctx->logger,
