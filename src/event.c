2e7b6f06507e30cb9d1102e5c6d6dc655c74533e
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-08-02 22:09:11 +0200

    event: Fix RX_DONE_DIRECT SDU decref

--- src/event.c
+++ src/event.c
@@ -27,6 +27,7 @@ static void event_dealloc(struct rlc_sched_item *item)
         switch (event->type) {
         case RLC_EVENT_RX_DONE_DIRECT:
                 gabs_pbuf_decref(*event->rx_done_direct.buf);
+                break;
         default:
                 rlc_sdu_decref(event->sdu);
         }
